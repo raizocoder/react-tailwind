7459ced19fdc70d7e6c87b523f207f2d8c817e96
Rohit Kumar <114095517+raizocoder@users.noreply.github.com>
2024-01-26 13:50:01 +0530

    Add files via upload

---
+++ 0tailwindcss/src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="text-center text-3xl text-yellow-400 font-mono font-semibold bg-gray-700 underline uppercase  ">
+        Hello
+      </h1>
+      <p className="bg-amber-200 text-[20px] w-28 h-12 rounded-md m-5 p-2 border-solid border-2 border-red-400">
+        world
+      </p>
+      <button className="text-2xl border-solid border-2 border-black p-3 rounded-lg bg-yellow-400 hover:bg-yellow-600 hover:w-28 hover:text-blue-300">Click</button>
+    </>
+  );
+};
+
+export default App;
